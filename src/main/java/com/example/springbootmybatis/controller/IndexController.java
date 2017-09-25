e7f7988323348b1e253e30824a4d62773ccbecfc
lhp <sky@qegoo.cn>
2017-09-25 18:00:04 +0800

    更换数据源地址

--- src/main/java/com/example/springbootmybatis/controller/IndexController.java
+++ src/main/java/com/example/springbootmybatis/controller/IndexController.java
@@ -1,17 +1,17 @@
 package com.example.springbootmybatis.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by lhp on 17/9/12.
  */
-@RestController
+@Controller
 public class IndexController {
 
     @RequestMapping("/")
     public String index() {
-        return "hello, world.";
+        return "redirect:/user/list";
     }
 
 }
